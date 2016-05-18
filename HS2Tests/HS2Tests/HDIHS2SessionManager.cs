88c79a1ba1a6ebe13027e46d966854161da37e2a
kirankumarkolli <kirankk@microsoft.com>
2016-05-17 17:09:42 -0700

    SessionManager ability to execute queries

--- HS2Tests/HS2Tests/HDIHS2SessionManager.cs
+++ HS2Tests/HS2Tests/HDIHS2SessionManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HS2Tests
@@ -12,12 +13,16 @@ namespace HS2Tests
         protected int NumOfSessions { get; set; }
 
         protected List<HDIHS2Session> Sessions { get; set; }
+        protected int CurrentActiveSessions { get; set; }
+        protected int CurrentActiveSessionsLimit { get; set; }
+        protected object ActiveSessionLock { get; set; }
 
         public HDIHS2SessionManager(string connectionString, int sessionCount)
         {
             this.ConnectionString = connectionString;
             this.NumOfSessions = sessionCount;
             this.Sessions = new List<HDIHS2Session>();
+            this.ActiveSessionLock = new object();
         }
 
         /// <summary>
@@ -25,7 +30,23 @@ namespace HS2Tests
         /// </summary>
         public void RunQuery(string query, int parallism, TimeSpan duration)
         {
+            this.CurrentActiveSessions = 0;
+            this.CurrentActiveSessionsLimit = parallism;
+
             // Synchorous execution till duration 
+            var tokenSource = new CancellationTokenSource();
+            var executors = new List<Task>();
+            foreach (var e in this.Sessions)
+            {
+                executors.Add(ExecuteSession(e, query, tokenSource.Token));
+            }
+
+            Task.Delay(duration).Wait();
+            tokenSource.Cancel();
+
+            Task.WaitAll(executors.ToArray());
+
+            // TODO: Collect the executor (TBD) results
         }
 
         protected void Init()
@@ -36,6 +57,49 @@ namespace HS2Tests
                 this.Sessions.Add(new HDIHS2Session(this.ConnectionString));
             }
         }
+
+        protected async Task ExecuteSession(HDIHS2Session session, string query, CancellationToken token)
+        {
+            while(! token.IsCancellationRequested)
+            {
+                if (this.AmIInLimits())
+                {
+                    try
+                    {
+                        session.ExecuteQuery(query);
+                    }
+                    finally
+                    {
+                        this.Release();
+                    }
+
+                }
+                await Task.Delay(TimeSpan.FromSeconds(10));
+            }
+        }
+
+        protected void Release()
+        {
+            lock (this.ActiveSessionLock)
+            {
+                this.CurrentActiveSessions--;
+            }
+        }
+
+        protected bool AmIInLimits()
+        {
+            lock(this.ActiveSessionLock)
+            {
+                if(this.CurrentActiveSessions < this.CurrentActiveSessionsLimit)
+                {
+                    this.CurrentActiveSessions++;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
 
         #region IDisposable Support
         private bool disposedValue = false; // To detect redundant calls
