4131b1ab22ea8e024d90b6cac3aaaae8c43cb71b
RajaKumari Kolli <rajakumari.kolli@gmail.com>
2016-06-06 22:39:02 -0700

    DAg edges handling (array or single)

---
+++ tezinsights/hive_20160607040030_b737b575-7da1-4f62-88dd-2454cfdeb6c5.js
@@ -0,0 +1,5 @@
+<script language="javascript" type="text/javascript" src="hive_20160607040030_b737b575-7da1-4f62-88dd-2454cfdeb6c5\query.js"></script>
+<script language="javascript" type="text/javascript" src="hive_20160607040030_b737b575-7da1-4f62-88dd-2454cfdeb6c5\dag.js"></script>
+<script language="javascript" type="text/javascript" src="hive_20160607040030_b737b575-7da1-4f62-88dd-2454cfdeb6c5\vertices.js"></script>
+<script language="javascript" type="text/javascript" src="hive_20160607040030_b737b575-7da1-4f62-88dd-2454cfdeb6c5\tasks.js"></script>
+<script language="javascript" type="text/javascript" src="hive_20160607040030_b737b575-7da1-4f62-88dd-2454cfdeb6c5\tasks_attempts.js"></script>
